02c59f4927be5ee4e47f8fec1810315c427add5f
David Scholberg <recombinant.vector@gmail.com>
2015-07-19 13:04:22 -0400

    updated style based on checkpatch.pl

--- merandom.c
+++ merandom.c
@@ -1,12 +1,13 @@
-// TODO: enable write operation on device file to set seed value?
-// TODO: find out where file_operations interfaces are documented
-// TODO: look into platform portability: http://kernelnewbies.org/WritingPortableDrivers
+/* TODO: enable write operation on device file to set seed value? */
+/* TODO: find out where file_operations interfaces are documented */
+/* TODO: look into platform portability:
+ * http://kernelnewbies.org/WritingPortableDrivers */
 
 #include <linux/module.h>
 #include <linux/miscdevice.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 static void merandom_fill_rb(char *, size_t);
 static uint64_t merandom_prng(void);
@@ -19,20 +20,18 @@ static ssize_t merandom_read(struct file *, char *, size_t, loff_t *);
 static ssize_t merandom_write(struct file *, const char *, size_t, loff_t *);
 
 #define MERANDOM_DEV_NAME "merandom"
-#define MERANDOM_BUF_LEN 16 * sizeof (uint64_t)
+#define MERANDOM_BUF_LEN (16 * sizeof(uint64_t))
 #define MERANDOM_STATE_BUF_LEN 16
 
-static int device_open = 0;       // is device open?
-//static unsigned char seed;
-//static unsigned char rc;          // current random character
+static int device_open;       /* is device open? */
 
 uint64_t rand_state[MERANDOM_STATE_BUF_LEN];
 int rand_state_p;
 
-static char rb[MERANDOM_BUF_LEN]; // buffer with random data
-static char *rb_ptr;              // pointer to current rb position
+static char rb[MERANDOM_BUF_LEN]; /* buffer with random data */
+static char *rb_ptr;              /* pointer to current rb position */
 
-static struct file_operations fops = {
+static const struct file_operations fops = {
 	.owner = THIS_MODULE,
 	.read = merandom_read,
 	.write = merandom_write,
@@ -55,13 +54,12 @@ static void merandom_fill_rb(char *rb_ptr, size_t len)
 	uint64_t rn;
 
 	while (len) {
-		//rc = (rc * 7) % 11;
 		rn = merandom_prng();
-		// TODO: make this a for loop so that buf len doesn't matter
-		*((uint64_t*) rb_ptr) = rn;
+		/* TODO: make this a for loop so that buf len doesn't matter */
+		*((uint64_t *) rb_ptr) = rn;
 
-		rb_ptr += sizeof (uint64_t);
-		len -= sizeof (uint64_t);
+		rb_ptr += sizeof(uint64_t);
+		len -= sizeof(uint64_t);
 	}
 }
 
@@ -69,20 +67,23 @@ static void merandom_fill_rb(char *rb_ptr, size_t len)
  * PRNG taken from https://en.wikipedia.org/wiki/Xorshift#Xorshift.2A
  * This is an implementation of xorshift1024*
  */
-static uint64_t merandom_prng(void) {
+static uint64_t merandom_prng(void)
+{
 	uint64_t s0 = rand_state[rand_state_p];
 	uint64_t s1 = rand_state[rand_state_p = (rand_state_p + 1) & 15];
+
 	s1 ^= s1 << 31;
 	s1 ^= s1 >> 11;
 	s0 ^= s0 >> 30;
-	return (rand_state[rand_state_p] = s0 ^ s1) * 1181783497276652981ULL; 
+	return (rand_state[rand_state_p] = s0 ^ s1) * 1181783497276652981ULL;
 }
 
-static void merandom_seed(void) {
+static void merandom_seed(void)
+{
 	uint64_t i;
-	for (i = 0; i < MERANDOM_STATE_BUF_LEN; i++) {
+
+	for (i = 0; i < MERANDOM_STATE_BUF_LEN; i++)
 		rand_state[i] = i + 1;
-	}
 
 	rand_state_p = i + 1;
 }
@@ -103,9 +104,9 @@ static int merandom_release(struct inode *inode, struct file *file)
 {
 	device_open--;
 
-	/* 
+	/*
 	 * Decrement the usage count, or else once you opened the file, you'll
-	 * never get get rid of the module. 
+	 * never get get rid of the module.
 	 */
 	module_put(THIS_MODULE);
 
@@ -115,21 +116,21 @@ static int merandom_release(struct inode *inode, struct file *file)
 static ssize_t merandom_read(struct file *filp,	/* see include/linux/fs.h   */
 			   char *buf,		/* buffer to fill with data */
 			   size_t len,		/* length of the buffer     */
-			   loff_t * off)
+			   loff_t *off)
 {
-	ssize_t bytes_read = len; // always output len bytes
+	ssize_t bytes_read = len; /* always output len bytes */
 
 	while (len) {
-		// if we run out of random data, get more
-		if (rb_ptr == rb + sizeof rb) {
-			merandom_fill_rb(rb, sizeof rb);
+		/* if we run out of random data, get more */
+		if (rb_ptr == rb + sizeof(rb)) {
+			merandom_fill_rb(rb, sizeof(rb));
 			rb_ptr = rb;
 		}
-		/* 
-		 * The buffer is in the user data segment, not the kernel 
-		 * segment so "*" assignment won't work.  We have to use 
+		/*
+		 * The buffer is in the user data segment, not the kernel
+		 * segment so "*" assignment won't work.  We have to use
 		 * put_user which copies data from the kernel data segment to
-		 * the user data segment. 
+		 * the user data segment.
 		 */
 		put_user(*rb_ptr, buf);
 
@@ -142,9 +143,9 @@ static ssize_t merandom_read(struct file *filp,	/* see include/linux/fs.h   */
 }
 
 static ssize_t
-merandom_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+merandom_write(struct file *filp, const char *buff, size_t len, loff_t *off)
 {
-	printk(KERN_ALERT "%s: write operation on /dev/%s not supported\n",
+	pr_alert("%s: write operation on /dev/%s not supported\n",
 			MERANDOM_DEV_NAME,
 			MERANDOM_DEV_NAME);
 	return -EINVAL;
@@ -153,26 +154,25 @@ merandom_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 static int __init merandom_init(void)
 {
 	int ret;
-	ret= misc_register(&merandom_miscdev);
+
+	ret = misc_register(&merandom_miscdev);
 	if (ret) {
 		pr_err("%s: misc_register failed: %d\n", MERANDOM_DEV_NAME,
 				ret);
 		return ret;
 	}
 
-	printk(KERN_INFO "%s: registered\n", MERANDOM_DEV_NAME);
-	printk(KERN_INFO "%s: created device file /dev/%s\n",
+	pr_info("%s: registered\n", MERANDOM_DEV_NAME);
+	pr_info("%s: created device file /dev/%s\n",
 			MERANDOM_DEV_NAME, MERANDOM_DEV_NAME);
 
-	// TODO: seed with actual random value
+	device_open = 0;
+
+	/* TODO: seed with actual random data */
 	merandom_seed();
-	//seed = 34;
-	//rc = seed;
 
-	//printk(KERN_INFO "%s: seeded with value %u\n", MERANDOM_DEV_NAME, seed);
-
-	// fill buffer with new random data and set pointer
-	merandom_fill_rb(rb, sizeof rb);
+	/* fill buffer with new random data and set pointer */
+	merandom_fill_rb(rb, sizeof(rb));
 	rb_ptr = rb;
 
 	return 0;
@@ -181,14 +181,15 @@ static int __init merandom_init(void)
 static void __exit merandom_exit(void)
 {
 	int ret;
+
 	ret = misc_deregister(&merandom_miscdev);
 	if (ret) {
 		pr_err("%s: misc_deregister failed: %d\n", MERANDOM_DEV_NAME,
 				ret);
 	}
 	else {
-		printk(KERN_INFO "%s: unregistered\n", MERANDOM_DEV_NAME);
-		printk(KERN_INFO "%s: deleted /dev/%s\n",
+		pr_info("%s: unregistered\n", MERANDOM_DEV_NAME);
+		pr_info("%s: deleted /dev/%s\n",
 				MERANDOM_DEV_NAME,
 				MERANDOM_DEV_NAME);
 	}
